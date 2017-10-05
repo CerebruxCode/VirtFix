014aff033957d82fbecb20ae8c531beac3e95cea
billniakas <billniakas@gmail.com>
2017-10-06 00:03:09 +0300

    Bash script update

--- virtfix.sh
+++ virtfix.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 if [ $EUID -ne 0 ] ; then
     clear
-    echo ""
-    echo "virtfix must be run as root, play it again Sam" 1>&2
-    echo ""
+    echo "virtfix must be run as root, play it again Sam";
     sleep 2
     exit 1
 fi
-
-sleep 5 
+sleep 1
+if [ ! -f "/usr/bin/virtualbox" ];
+then
+	sleep 1
+	echo "No VirtualBox Installation Present"
+	sleep 1
+	echo "exiting..."
+	exit 0
+fi
 
 echo "Fixing VirtualBox"
 sleep 2
 sudo apt install --reinstall virtualbox-dkms
 sudo modprobe vboxdrv
-
-read -p "Do you want to run virtualbox? [(y)es]/[(n)o]" -n 1 -r
-echo    # (optional) move to a new line
+read -rp "Do you want to run virtualbox? [(y)es]/[(n)o]" 
+echo  
 if [[ $REPLY =~ ^[Yy]$ ]]
 	then
-    		virtualbox &
-		exit
+    		virtualbox & 
+		exit 0;
 fi
 
 if [[ $REPLY =~ ^[Nn]$ ]]
 	then
-	    	exit
+	    	exit 0;
 fi
 
